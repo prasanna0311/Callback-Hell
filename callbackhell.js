07bb5723432e95fdc6a5dbf8a5d8a471deeefc16
prasanna0311 <83944299+prasanna0311@users.noreply.github.com>
2021-06-15 23:51:58 +0530

    Add files via upload

---
+++ callbackhell.js
@@ -0,0 +1,35 @@
+let countdown = 10;
+setTimeout(function () {
+  document.getElementById("display").innerHTML = countdown--;
+  setTimeout(function () {
+    document.getElementById("display").innerHTML = countdown--;
+    setTimeout(function () {
+      document.getElementById("display").innerHTML = countdown--;
+      setTimeout(function () {
+        document.getElementById("display").innerHTML = countdown--;
+        setTimeout(function () {
+          document.getElementById("display").innerHTML = countdown--;
+          setTimeout(function () {
+            document.getElementById("display").innerHTML = countdown--;
+            setTimeout(function () {
+              document.getElementById("display").innerHTML = countdown--;
+              setTimeout(function () {
+                document.getElementById("display").innerHTML = countdown--;
+                setTimeout(function () {
+                  document.getElementById("display").innerHTML = countdown--;
+                  setTimeout(function () {
+                    document.getElementById("display").innerHTML = countdown--;
+                    setTimeout(function () {
+                      document.getElementById("display").innerHTML =
+                        "Happy Independence Day";
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
